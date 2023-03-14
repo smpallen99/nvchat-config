227c323980cabe520878d66dfba7e20caf5a1ea1
Stephen Pallen <steve.pallen@emetrotel.com>
2023-03-14 16:39:09 -0400

    add emmet plugin

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "elixirls", "volar" }
+local servers = { "html", "cssls", "tsserver", "clangd", "elixirls", "volar", "emmet_ls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -17,6 +17,16 @@ lspconfig.elixirls.setup {
   cmd = { vim.fn.expand("~/.local/share/nvim/mason/packages/elixir-ls/language_server.sh") }
 }
 
+-- emmet 
+
+lspconfig.emmet_ls.setup {
+  cmd = {"emmet-ls", "--stdio"},
+  filetypes = { "html", "css", "sass", "scss", "vue" },
+  root_dir = function()
+    return vim.loop.cwd()
+  end,
+  settings = {},
+}
 -- :lua vim.lsp.buf.format()
 
 -- 
