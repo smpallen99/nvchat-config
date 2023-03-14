54b5236f95c99765469dae38044009b1cfd2cc98
Stephen Pallen <steve.pallen@emetrotel.com>
2023-03-14 18:55:27 -0400

    finished whichkey

--- mappings.lua
+++ mappings.lua
@@ -3,13 +3,13 @@ local M = {}
 
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["|"] = { ":vsplit<CR>", "new vertical split", opts = { nowait = true }},
-    ["-"] = { ":split<CR>", "new horizontal split", opts = { nowait = true }},
-    ["<leader>r"] = { ":b#<CR>", "new horizontal split", opts = { nowait = true }},
-    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
-    ["<leader>x"] = { ":lua vim.lsp.buf.format() <CR>", "format buffer", { nowait = true }},
-  },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- ["|"] = { ":vsplit<CR>", "new vertical split", opts = { nowait = true }},
+    -- ["-"] = { ":split<CR>", "new horizontal split", opts = { nowait = true }},
+    -- ["<leader>r"] = { ":b#<CR>", "new horizontal split", opts = { nowait = true }},
+    -- ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle nvimtree" },
+    -- ["<leader>x"] = { ":lua vim.lsp.buf.format() <CR>", "format buffer", { nowait = true }},
+   },
 }
 
 -- :lua vim.lsp.buf.format()
