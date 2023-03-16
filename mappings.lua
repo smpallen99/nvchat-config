d54ad08fa353445db355f3be964f8415f08d93f4
Stephen Pallen <steve.pallen@emetrotel.com>
2023-03-16 15:40:47 -0400

    add which_key config

--- mappings.lua
+++ mappings.lua
@@ -1,9 +1,39 @@
 ---@type MappingsTable
 local M = {}
 
+M.disabled = {
+  n = {
+      ["<leader>h"] = "",
+      ["<leader>ff"] = "",
+      ["<leader>fa"] = "",
+      ["<leader>fb"] = "",
+      ["<leader>fh"] = "",
+      ["<leader>fo"] = "",
+      ["<leader>fw"] = "",
+      ["<leader>f"] = "",
+      ["<leader>cc"] = "",
+      ["<leader>ch"] = "",
+      ["<leader>cm"] = "",
+      ["<leader>v"] = "",
+      ["<leader>x"] = "",
+      ["<leader>gt"] = "",
+      ["<leader>pt"] = "",
+      ["<leader>rn"] = "",
+      ["<leader>th"] = "",
+      ["<leader>wk"] = "",
+      ["<leader>wK"] = "",
+      ["<leader>b"] = "",
+      ["<leader>n"] = "",
+  }
+}
+
 M.general = {
   n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["|"] = { ":vsplit<CR>", "new vertical split", opts = { nowait = true }},
+    ["-"] = { ":split<CR>", "new horizontal split", opts = { nowait = true }},
+    ["<leader>e"] = { "<cmd> NvimTreeToggle <CR>", "Explorer" },
+    ["gd"] = { "<cmd>Telescope lsp_definitions<CR>", opts = { nowait = true }},
   },
 }
 
