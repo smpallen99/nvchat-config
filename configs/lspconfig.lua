710834dedbda608e3e2c99331b70eca2a0eed4d9
Stephen Pallen <steve.pallen@emetrotel.com>
2023-03-16 13:37:22 -0400

    elixirls working

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "volar" }
+local servers = { "html", "cssls", "tsserver", "volar", "elixirls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -13,5 +13,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+lspconfig.elixirls.setup {
+  cmd = { vim.fn.expand("~/.local/share/nvim/mason/packages/elixir-ls/language_server.sh") }
+}
 -- 
 -- lspconfig.pyright.setup { blabla}
